a955e91fac06d9d0de41dc0a34f0ff002aa14be8
Jiri <jiri@cberryit.com>
2026-09-02 18:51:02 -0400

    Release 26.22.0
    Update IRKit.xcframework binary target to 26.22.0.
    Asset URL: https://github.com/irdb-holdings/irkit-releases/releases/download/26.22.0/IRKit.xcframework.zip

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IRKit",
-            url: "https://github.com/irdb-holdings/irkit-releases/releases/download/26.21.0/IRKit.xcframework.zip",
-            checksum: "c54f31f80d093eca5a445fec3c43891312ab0dd062bef7509a978f0b28c49cfc"
+            url: "https://github.com/irdb-holdings/irkit-releases/releases/download/26.22.0/IRKit.xcframework.zip",
+            checksum: "dbb97d3f79cc331d7e32f9ae72c920658364bd746392b6e010580550a407bd16"
         )
     ]
 )
